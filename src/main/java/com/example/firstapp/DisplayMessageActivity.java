8c1321f5e09ad16b598e93b8d1e77896f8688de7
carrolltechnical <32939949+carrolltechnical@users.noreply.github.com>
2017-11-26 13:07:03 -0800

    changed edittext in activity_main

--- src/main/java/com/example/firstapp/DisplayMessageActivity.java
+++ src/main/java/com/example/firstapp/DisplayMessageActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
@@ -23,15 +24,18 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-
+    }
+    public void sendMessage(View view) {
         // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
+        //Intent intent = getIntent();
+        Intent intent = new Intent(this, DisplayMessageActivity2.class);
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.textView);
+        TextView textView = (TextView) findViewById(R.id.smsNumber);
         textView.setText(message);
+        String textThis = String.valueOf(R.id.smsNumber);
         SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage("5555555555", null, "test sms message", null, null);
+        smsManager.sendTextMessage(textThis, null, "test sms message", null, null);
     }
 }
--- src/main/java/com/example/firstapp/MainActivity.java
+++ src/main/java/com/example/firstapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
+    public void toSendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
